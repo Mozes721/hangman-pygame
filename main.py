b40703a390761e1147c2a3c80e9798826d8ce3b2
Mozes721 <Richard.Taujenis@gmail.com>
2021-08-09 17:53:52 +0200

    images attached

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame, sys
+import random 
+import time
+from constants import *
+pygame.init()
+
+clock = pygame.time.Clock()
+
+gameDisplay=pygame.display.set_mode((h,w))
+
+pygame.display.set_caption('Hangman')
+gameDisplay.fill(BOARDER)
+pygame.draw.rect(gameDisplay, WHITEBOARD, pygame.Rect(22, 20, 650, 430))
+
+img1 = pygame.image.load('img/hang1.png')
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    gameDisplay.blit(img1, [100, 120])	    
+    pygame.display.update()
