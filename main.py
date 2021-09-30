0b4c5dc4e2f61e904c7c806c9bbaebe8a94e7a75
Mozes721 <Richard.Taujenis@gmail.com>
2021-09-30 18:11:37 +0200

    space bar press to restart game

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import pygame
 import random 
 import time
@@ -6,7 +5,6 @@ from constants import *
 pygame.init()
 pygame.font.init()
  
-  
 gameDisplay=pygame.display.set_mode((h,w))
 word = None
 
@@ -14,11 +12,10 @@ pygame.display.set_caption('Hangman')
 gameDisplay.fill(BOARDER)
 pygame.draw.rect(gameDisplay, WHITEBOARD, pygame.Rect(22, 20, 650, 430))
 
-###text object render
+#text object render
 def text_objects(text, font):
     textSurface = font.render(text, True, BLACK)
     return textSurface, textSurface.get_rect()
-
 
 #game text display
 def game_texts(text, x, y):
@@ -34,21 +31,6 @@ def finish_text(text, x, y):
     gameDisplay.blit(TextSurf, TextRect)
  
     pygame.display.update()    
-#button display
-def button(msg, x, y, w, h, ic, ac, action=None):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x + w > mouse[0] > x and y + h > mouse[1] > y:
-        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
-        if click[0] == 1 != None:
-            action()
-    else:
-        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
-
-    TextSurf, TextRect = text_objects(msg, btn_font)
-    TextRect.center = ((x + (w/2)), (y + (h/2)))
-    gameDisplay.blit(TextSurf, TextRect)
-
 
 def redraw_window():
     global word   
@@ -72,7 +54,7 @@ def redraw_window():
     TextRect.center = (350, 400)
     gameDisplay.blit(TextSurf, TextRect)
     
-    TextSurf, TextRect = text_objects("Wrong Pressed : %s" % wrong_guesses , wrong_guesses_font)
+    TextSurf, TextRect = text_objects(" Wrongly Pressed : %s" % wrong_guesses , wrong_guesses_font)
     TextRect.center = (450, 50)
     gameDisplay.blit(TextSurf, TextRect)
     
@@ -89,26 +71,14 @@ def finished_game():
     for ltr in word:
         if ltr not in guesses:
             won = False
-    
     if won:
-        finish_text("You Won :)", 400, 150)
+        finish_text("You Won :)", 400, 120)
         time.sleep(2)
-        pygame.draw.rect(gameDisplay, WHITEBOARD, pygame.Rect(22, 20, 650, 430))
-        reset()
-        #button("PLAY AGAIN?", 400, 100, 150, 50, light_slat, bright_green, reset)
-        #button("EXIT", 400, 200, 150, 50, light_slat, dark_red, sys.exit())
-       
-
+     
     if limbs == 6:
-        finish_text("You Lost :(", 400, 150)
+        finish_text("You Lost :(", 400, 120)
         time.sleep(2)
-        pygame.draw.rect(gameDisplay, WHITEBOARD, pygame.Rect(22, 20, 650, 430))
-        reset()
-        #button("PLAY AGAIN?", 400, 100, 150, 50, light_slat, bright_green, reset)
-        #button("EXIT", 400, 200, 150, 50, light_slat, dark_red, sys.exit())
-    pygame.display.update()
-        
-
+ 
 def reset():
     global limbs
     global guesses
@@ -119,15 +89,14 @@ def reset():
     guesses = []
     wrong_guesses = []
     word = random_word()
-    print(word)
-
 
 word = random_word()
 inGame = True
 
 
-print(word)
+#print(word)
 while inGame:
+    game_texts("SPACE TO START NEW GAME", 350, 300)
     redraw_window()
     finished_game()
     pygame.time.delay(10)
@@ -136,6 +105,12 @@ while inGame:
         if event.type == pygame.QUIT:
             pygame.quit()
         elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                gameDisplay=pygame.display.set_mode((h,w))
+                word = None
+                gameDisplay.fill(BOARDER)
+                pygame.draw.rect(gameDisplay, WHITEBOARD, pygame.Rect(22, 20, 650, 430))
+                reset()
             if event.key in range(pygame.K_a, pygame.K_z + 1):
                 if event.unicode in guesses or event.unicode in wrong_guesses:
                     game_texts("You already pressed it!", 450, 230)
@@ -150,4 +125,6 @@ while inGame:
                     pygame.draw.rect(gameDisplay, WHITEBOARD, pygame.Rect(22, 20, 600, 230))
                 else:
                     guesses += event.unicode
-            print(guesses)
+                
+        
+    
